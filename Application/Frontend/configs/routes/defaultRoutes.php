9004c62094fa9ee2bb29f0420c9c029a59dc2852
Agustin Casiva <agustin@42mate.com>
2014-07-06 11:28:14 -0300

    Better assets handling, now moved all application assets to the application and added a spetial assets controller to read and server the app assets

--- Application/Frontend/configs/routes/defaultRoutes.php
+++ Application/Frontend/configs/routes/defaultRoutes.php
@@ -1,13 +1,1 @@
 <?php
-
-add_route('get', '/', array(
-    'controller' => new Towel\Controller\BaseController(),
-    'action' => 'index',
-));
-
-if ($appConfig['debug']) {
-    get_app()->silex()->error(function (\Exception $e)  {
-        $controller = new Towel\Controller\BaseController();
-        return $controller->routeError($e);
-    });
-}
