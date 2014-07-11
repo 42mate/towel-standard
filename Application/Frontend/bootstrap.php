dbe08455e25b9cd5bc19ae3d4cc56ab2a5afda82
Agustin Casiva <agustin@42mate.com>
2014-07-10 23:30:38 -0300

    Updated jquery, ui improvements for Frontend

--- Application/Frontend/bootstrap.php
+++ Application/Frontend/bootstrap.php
@@ -3,3 +3,6 @@
 require_once dirname(__FILE__) . '/../../vendor/autoload.php';
 require_once dirname(__FILE__) . '/configs/config.php';
 require_once APP_FW_DIR . '/bootstrap.php';
+
+//Init Routes
+add_app_routes('Frontend');
