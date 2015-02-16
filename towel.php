e55dbab111a999c94fd08cebab869249456a2496
Casiva Agustin <casivaagustin@gmail.com>
2015-02-16 10:55:42 -0300

    Update towel.php
    Improvements for Console execution, reusing the bootstrap of towel instead of write some new stuff here (dup code), reusing app configuration, again, to avoid dup code.

--- towel.php
+++ towel.php
@@ -1,49 +1,39 @@
 <?php
 
-//Define Constants
-define('WORK_DIR', getcwd());
-
 //Require Files
 require_once dirname(__FILE__) . '/vendor/autoload.php';
+require_once  './Application/config.php';
+require_once APP_FW_DIR . '/bootstrap.php';
 
-if (file_exists(WORK_DIR . '/bootstrap.php')) {
-    require_once WORK_DIR . '/config.php';
-    $appRun = true;
-} else {
-    $appRun = false;
-    require_once dirname(__FILE__) . '/vendor/42mate/towel/src/Towel/Console/Configs/config.php';
-}
-foreach (glob(APP_FW_DIR . "/includes/*.inc.php") as $includeFiles) {
-    require_once "$includeFiles";
-}
-//Set Console Application
-global $silex;
-$silex = new Silex\Application();
-$silex['debug'] = $appConfig['debug'];
-if (array_key_exists('doctrine', $appConfig)) {
-    $silex->register(new Silex\Provider\DoctrineServiceProvider(), $appConfig['doctrine']);
-}
-$silex->register(new Silex\Provider\TwigServiceProvider(), $appConfig['twig']);
-$silex->register(new Silex\Provider\UrlGeneratorServiceProvider());
-
-$silex->register(new Knp\Provider\ConsoleServiceProvider(), array(
-    'console.name'              => 'TowelConsole',
-    'console.version'           => '1.0.0',
-    'console.project_directory' => COMMANDS_DIR,
-));
-
-//Init Twig Functions
-require_once APP_FW_DIR . "/includes/twig/twig_functions.inc.php";
-
-//Init Twig Filters
-require_once APP_FW_DIR . "/includes/twig/twig_filters.inc.php";
+$silex->register(
+    new Knp\Provider\ConsoleServiceProvider(),
+    array(
+        'console.name' => 'TowelConsole',
+        'console.version' => '1.0.0',
+        'console.project_directory' => '.',
+    )
+);
 
 $consoleApp = $silex['console'];
-foreach (scandir(COMMANDS_DIR) as $file) {
-    $pathinfo = pathinfo($file);
-    if (substr($pathinfo['filename'], -7) === 'Command') {
-        $command = COMMANDS_NAMESPACE . '\\' . $pathinfo['filename'];
-        $consoleApp->add(new $command());
+$apps = \Towel\Towel::getApps();
+
+foreach ($apps as $app) {
+    $commandsDir = $app['path'] . '/Command';
+    $commands = glob($commandsDir.'/*.php');
+
+    foreach ($commands as $command) {
+        $commandFile = basename($command);
+        $commandClass = str_replace('.php', '', $commandFile);
+
+        if ($app['name'] == 'Towel') {
+            $commandClass = '\\' . $app['name'] . '\Command\\' . $commandClass;
+        } else {
+            $commandClass = '\Application\\' . $app['name'] . '\Command\\' . $commandClass;
+        }
+
+        $consoleApp->add(new $commandClass());
     }
 }
+
+
 $consoleApp->run();
