fa90ee9e2c833eda7862db569afe05e77d36a135
Agustin Casiva <agustin@42mate.com>
2015-03-26 15:44:59 -0300

    Improvements in Console to support environments

--- towel.php
+++ towel.php
@@ -1,21 +1,33 @@
 <?php
 
+use Symfony\Component\Console\Input\ArgvInput;
+use Symfony\Component\Console\Output\ConsoleOutput;
+
+
 //Require Files
 require_once dirname(__FILE__) . '/vendor/autoload.php';
+
+$input = new ArgvInput();
+$output = new ConsoleOutput();
+
+$env = $input->getParameterOption(array('--env', '-e'), getenv('TOWEL_ENV') ?: 'local');
+$_SERVER['HTTP_HOST'] = $env;
+
+
 require_once  './Application/config.php';
 require_once APP_FW_DIR . '/bootstrap.php';
 
 $silex->register(
     new Knp\Provider\ConsoleServiceProvider(),
     array(
-        'console.name' => 'TowelConsole',
+        'console.name' => 'Towel Console - Environment : ' . $env,
         'console.version' => '1.0.0',
         'console.project_directory' => '.',
     )
 );
 
 $consoleApp = $silex['console'];
-$apps = \Towel\Towel::getApps();
+$apps = Towel\Towel::getApps();
 
 foreach ($apps as $app) {
     $commandsDir = $app['path'] . '/Command';
@@ -34,6 +46,5 @@ foreach ($apps as $app) {
         $consoleApp->add(new $commandClass());
     }
 }
-
 
 $consoleApp->run();
